e43090fb78c3c9fa9468658c1744f5802ef8a723
Simon Podlipsky <simon@podlipsky.net>
2023-03-13 11:36:44 +0100

    feat: remove deprecated `never()`

--- src/functions_include.php
+++ src/functions_include.php
@@ -6,5 +6,4 @@ require_once __DIR__ . '/absurd.php';
 require_once __DIR__ . '/ds.php';
 require_once __DIR__ . '/generator.php';
 require_once __DIR__ . '/iterable.php';
-require_once __DIR__ . '/never.php';
 require_once __DIR__ . '/noop.php';
