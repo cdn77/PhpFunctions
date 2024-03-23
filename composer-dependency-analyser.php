e1582eeecbfde741159471e6a296f57744ac38c4
Simon Podlipsky <simon@podlipsky.net>
2024-03-23 21:19:59 +0100

    ci: use Composer dependency analyser (#43)
    - drop maglnet/composer-require-checker
    - drop icanhazstring/composer-unused

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$config = new Configuration();
+
+return $config
+    ->ignoreErrorsOnPackage('phpoption/phpoption', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
