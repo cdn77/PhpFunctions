f83ca40c97b2e21710d6f92e384c02d249e788d7
Simon Podlipsky <simon@podlipsky.net>
2024-04-18 15:45:58 +0200

    chore(deps): bump shipmonk/composer-dependency-analyser to v1.5 and scan src (#47)

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,9 +3,8 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
-use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
 
 return $config
-    ->ignoreErrorsOnPackage('phpoption/phpoption', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
+    ->addPathToScan(__DIR__ . '/src', isDev: false);
